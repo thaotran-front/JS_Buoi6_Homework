88002d5e8c78ed985b98f66c66f98c6f2d5a579c
tamartran <ttphuongthao27@gmail.com>
2022-03-26 23:54:50 +0700

    js_buoi6

---
+++ main.js
@@ -0,0 +1,11 @@
+// BÀI TẬP 1
+var sum = 0;
+var n = 0;
+var timSoNguyenDuongNhoNhat
+for(var i = 0; sum > 10000; i++){
+    sum += i;
+    n++
+}
+
+var result = timSoNguyenDuongNhoNhat;
+console.log(result);
